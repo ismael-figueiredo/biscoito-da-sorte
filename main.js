f341497933335af7cb2dac5b70561731f70bda87
“Ismael_figueiredoe” <maelio@live.coml>
2023-12-24 20:09:16 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const page1 = document.querySelector(".page-1")
+const page2 = document.querySelector(".page-2")
+const button = document.querySelector("#open-new")
+
+const cookieClosed = document.querySelector("#cookie-closed")
+
+function openPage2() {
+  page1.classList.add("hidden")
+  page2.classList.remove("hidden")
+}
+function openPage1() {
+  page1.classList.remove("hidden")
+  page2.classList.add("hidden")
+}
+
+cookieClosed.addEventListener("click", openPage2)
+button.addEventListener("click", openPage1)
