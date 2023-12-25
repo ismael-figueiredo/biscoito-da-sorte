16a1d84b180c30c95cfeab39af81d5732fe8ec9a
“Ismael_figueiredoe” <maelio@live.coml>
2023-12-24 21:48:23 -0300

    comit final

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-import { frases } from "./content.js"
+import { frases } from "./phrases.js"
 
 // váriaveis
 const page1 = document.querySelector(".page-1")
 const page2 = document.querySelector(".page-2")
 const button = document.querySelector("#open-new")
-const content = document.querySelector("#content")
-
 const cookieClosed = document.querySelector("#cookie-closed")
 
+// abre a página 2
 function openPage2() {
   page1.classList.add("hidden")
   page2.classList.remove("hidden")
 }
+cookieClosed.addEventListener("click", openPage2)
+
+//abre a página 1
 function openPage1() {
   page1.classList.remove("hidden")
   page2.classList.add("hidden")
 }
-
-cookieClosed.addEventListener("click", openPage2)
 button.addEventListener("click", openPage1)
 
 
-
+// escolhe uma frase aleatóriamente
 document.getElementById("cookie-closed").addEventListener("click", () => {
   const contentElement = document.getElementById("content")
 
